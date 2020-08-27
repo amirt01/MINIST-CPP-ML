13d06a9d90e6f36b53a0be2847405ad632d74cf0
amirt01 <amirtadros01@gmail.com>
2020-08-26 21:04:36 -0700

    final cleanup

--- KNN/src/knn.cpp
+++ KNN/src/knn.cpp
@@ -14,7 +14,7 @@
 double knn::calculate(std::vector<Data*>* set) {
   int count = 0;  // a count of the number of correct predictions
   int index = 0;  // the current index in the data set
-  for (auto& queryPoint : *set) {  // for each queryPoint in the data set
+  for (auto* queryPoint : *set) {  // for each queryPoint in the data set
     findKnearest(queryPoint);  // find the k nearest points to the queryPoint
     int prediction = predict();
     std::cout << prediction << " -> " << unsigned(queryPoint->getLabel()) << '\n';
@@ -40,10 +40,10 @@ knn::knn(DataHandler* dh, const int& val)
 
 void knn::findKnearest(Data* queryPoint) {
   // set the initial min_dist / previous_min_dist to be the maximum possible distance
-  double min_dist = std::numeric_limits<double>::max();
-  double previous_min_dist = min_dist;
+  double minDist = std::numeric_limits<double>::max();
+  double previousMinDist = minDist;
   // create a pointer to keep track of the current closest value we want to append to m_neighbors
-  Data* closest_ptr = nullptr;
+  Data* closestPtr = nullptr;
   // find the m_k closest features
   for (int i = 0; i < m_k; ++i) {
     if (i == 0) {  // for the first value
@@ -51,25 +51,25 @@ void knn::findKnearest(Data* queryPoint) {
         // calculate the distance from the queryPoint to that value
         double distance = queryPoint->calculateDistance(*feature);
         feature->setDistance(distance);  // store the calculated distance for future use
-        if (distance < min_dist) {  // check if this is the minimum distance
-          min_dist = distance;  // store the minimum distance
-          closest_ptr = feature;  // store the closest feature
+        if (distance < minDist) {  // check if this is the minimum distance
+          minDist = distance;  // store the minimum distance
+          closestPtr = feature;  // store the closest feature
         }
       }
     } else {  // for every value after the first value
       for (auto& feature : *m_trainingData) {  // check each feature in m_trainingData
         // collect the previously stored distance so we don't need to recalculate it
         double distance = feature->getDistance();
-        // only update closest_ptr if this is farther than the previous closest
-        if (distance > previous_min_dist && distance < min_dist) {
-          min_dist = distance;  // store the minimum distance
-          closest_ptr = feature;  // store the closest feature
+        // only update closestPtr if this is farther than the previous closest
+        if (distance > previousMinDist && distance < minDist) {
+          minDist = distance;  // store the minimum distance
+          closestPtr = feature;  // store the closest feature
         }
       }
     }
-    m_neighbors.push_back(closest_ptr);  // append the closest feature
-    previous_min_dist = min_dist;  // keep track of the previous min
-    min_dist = std::numeric_limits<double>::max();  // reset the min value
+    m_neighbors.push_back(closestPtr);  // append the closest feature
+    previousMinDist = minDist;  // keep track of the previous min
+    minDist = std::numeric_limits<double>::max();  // reset the min value
   }
 }
 
